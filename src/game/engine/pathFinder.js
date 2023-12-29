4c8a399f17592a5e7f4a83a4b0a5b6395974da89
Brian Zhou <brianhzhou@gmail.com>
2023-12-28 23:16:03 -0800

    implement A* path finding

--- src/game/engine/pathFinder.js
+++ src/game/engine/pathFinder.js
@@ -1,8 +1,12 @@
 import store from "../../store/redux";
+import { getElapsedTime } from "./animationLoop";
 import { CanvasTools } from "./canvasTools";
 import { Heap } from "./myHeap";
+import { Tile } from "./tile";
 // import { MAP_OFFSET, TILE_SIZE } from "./tileMap";
 import { Vector2 } from "./vector2";
+
+const STEP_TIMEOUT_MS = 0;
 
 export class PathFinder {
     constructor() {
@@ -26,6 +30,7 @@ export class PathFinder {
 
     startSearch = (state) => {
         this.pathTiles = {};
+        this.heap.clear();
         this.mapData = JSON.parse(JSON.stringify(state.mapData));
 
         // delete all tiles that are in delete animation
@@ -49,13 +54,85 @@ export class PathFinder {
 
         // insert starting position into frontier
         this.heap.insert(startPosition, 0, this.getHeuristic(startPosition));
+
+        this.stepIntervalId = setInterval(this.step, STEP_TIMEOUT_MS);
+    }
+
+    step = () => {
+        const heap = this.heap;
+
+        if (!heap.isEmpty()) {
+            const node = heap.remove();
+            const gridIndex = node.gridIndex;
+            this.createPathTile(gridIndex);
+
+            if (this.tileIsGoal(gridIndex)) {
+                this.endAlgorithmn();
+            } else {
+                this.addNeighbourToHeap(node);
+            }
+        } else {
+            this.endAlgorithmn();
+        }
+    }
+
+    endAlgorithmn() {
+        clearInterval(this.stepIntervalId);
+        console.log("ALGORITHMN COMPLETE!");
+        // todo: update redux state
+    }
+
+    addNeighbourToHeap(node) {
+        this.insertNeighbour(node, -1, 0);
+        this.insertNeighbour(node, 1, 0);
+        this.insertNeighbour(node, 0, -1);
+        this.insertNeighbour(node, 0, 1);
+    }
+
+    tileIsGoal(pos) {
+        const goals = this.mapData.goals;
+        if (! goals[pos.x]) return false;
+
+        return goals[pos.x][pos.y] && true;
+    }
+
+    insertNeighbour(node, x, y) {
+        const heap = this.heap;
+        const pathCost = node.pathCost + 0.99;
+        const gridIndex = node.gridIndex;
+        const pos = Vector2.add(gridIndex, new Vector2(x, y));
+
+        if (!this.isValidPath(pos)) return;
+
+        heap.insert(
+            pos,
+            pathCost,
+            this.getHeuristic(pos)
+        );
+        this.createPathTile(pos, "rgba(255, 255, 255, 0.25)");
+    }
+
+    createPathTile(pos, color = "rgba(255, 255, 0, 0.5)") {
+        const pathTiles = this.pathTiles;
+        if (!pathTiles[pos.x]) pathTiles[pos.x] = {};
+        pathTiles[pos.x][pos.y] = new Tile(
+            new Vector2(pos.x, pos.y),
+            "path",
+            getElapsedTime(),
+            color
+        );
     }
 
     update(dtSec, elapsedTimeSec) {
         if (!this.isSearching) return;
 
-        // if time > wait time, step the generic search algo
-        // make sure not to re-search nodes already on the tree!
+        const pathsTiles = this.pathTiles;
+
+        for (const gridX of Object.keys(pathsTiles)) {
+            for (const gridY of Object.keys(pathsTiles[gridX])) {
+                pathsTiles[gridX][gridY].update(dtSec, elapsedTimeSec);
+            }
+        }
     }
 
     getHeuristic(pos) {
@@ -68,7 +145,7 @@ export class PathFinder {
                     minHeuristic = dist;
                 }
             }
-        } 
+        }
 
         return minHeuristic;
     }
@@ -79,11 +156,30 @@ export class PathFinder {
 
     render() {
         if (!this.isSearching) return;
+        const pathsTiles = this.pathTiles;
 
-        for (const gridX of Object.keys(this.mapData.goals)) {
-            for (const gridY of Object.keys(this.mapData.goals[gridX])) {
-              console.log(gridX, gridY);
+        for (const gridX of Object.keys(pathsTiles)) {
+            for (const gridY of Object.keys(pathsTiles[gridX])) {
+                pathsTiles[gridX][gridY].render();
             }
-        }    
+        }
+    }
+
+    isValidPath(gridIndex) {
+        const mapData = this.mapData;
+        const pathTiles = this.pathTiles;
+        const wallTiles = this.mapData.tileData;
+        
+        if (wallTiles[gridIndex.x] && wallTiles[gridIndex.x][gridIndex.y]) return false;
+        if (pathTiles[gridIndex.x] && pathTiles[gridIndex.x][gridIndex.y]) return false;
+
+        const inBounds = (
+            !(gridIndex.x < 0) &&
+            !(gridIndex.y < 0) &&
+            gridIndex.x < mapData.width &&
+            gridIndex.y < mapData.height
+        );
+
+        return inBounds;
     }
 }
